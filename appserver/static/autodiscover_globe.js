dc20bbd111487590119d60ee14153f1a02f78323
hobbes3 <hobbes3@gmail.com>
2015-04-25 05:16:42 -0700

    Added boxplot and a tutorial.

--- appserver/static/autodiscover_globe.js
+++ appserver/static/autodiscover_globe.js
@@ -10,8 +10,8 @@ require.config({
     }
 });
 
-require(['splunkjs/mvc/simplexml/ready!'], function(){
-    require(['splunkjs/ready!'], function(){
+require(['splunkjs/mvc/simplexml/ready!'], function() {
+    require(['splunkjs/ready!'], function() {
         // The splunkjs/ready loader script will automatically instantiate all elements
         // declared in the dashboard's HTML.
     });
