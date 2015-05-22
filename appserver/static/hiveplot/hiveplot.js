4777d22934d0fa0ad7533046afc096ef9429d3ee
hobbes3 <hobbes3@gmail.com>
2015-05-22 01:27:54 -0400

    Archived the more complicated hive plot and created a simpler one. Still need to work on the text labeling.

--- appserver/static/hiveplot/hiveplot.js
+++ appserver/static/hiveplot/hiveplot.js
@@ -1,8 +1,8 @@
-// Hive plot D3.js code taken and modified from http://bost.ocks.org/mike/hive/ by Mike Bostock
+// Hive plot D3.js code taken and modified from http://bl.ocks.org/mbostock/2066415 by Mike Bostock
 
 define(function(require, exports, module) {
     var _ = require("underscore");
-    var d3 = require("../d3/d3");
+    var d3 = require("../d3/d3.hive");
     var SimpleSplunkView = require("splunkjs/mvc/simplesplunkview");
     require("css!./hiveplot.css");
 
@@ -13,8 +13,13 @@ define(function(require, exports, module) {
             "data": "preview",
             "height": 900,
             "src_field": "src",
+            "value_field": "value",
+            "property_field": "property",
             "dest_field": "dest",
-            "category_field": "category"
+            "category_field": "category",
+            "category_order": null,
+            "links_name": "links",
+            "nodes_name": "nodes"
         },
         output_mode: "json",
         initialize: function() {
@@ -38,26 +43,62 @@ define(function(require, exports, module) {
             var src_field = that.settings.get("src_field");
             var dest_field = that.settings.get("dest_field");
             var category_field = that.settings.get("category_field");
+            var category_order = that.settings.get("category_order");
 
-            var formatted_data = _(data)
+            var formatted_data = {};
+
+            var a = _(data).first();
+
+            var categories = category_order || _(data).chain().pluck("region").uniq().compact().value();
+            var min = _(data).chain().map(function(o) { return parseFloat(o.population); }).min().value();
+            var max = _(data).chain().map(function(o) { return parseFloat(o.population); }).max().value();
+            var range = max - min;
+
+            var nodes_info = _(data).filter(function(o) { return o.population; });
+
+            var nodes = _(data)
                 .chain()
-                .groupBy(src_field)
-                .map(function(v, k) {
-                    var obj = {
-                        src: k,
-                        category: v[0][category_field],
-                        dests: _(v).pluck(dest_field)
+                .uniq(function(o) { return o.from; })
+                .map(function(o) {
+                    var name = o.from;
+                    var region = _(nodes_info).findWhere({from: name}).region;
+                    var population = parseFloat(_(nodes_info).findWhere({from: name}).population);
+
+                    return {
+                        name: name,
+                        x: categories.indexOf(region),
+                        y: (population - min)/range,
+                        population: population,
+                        region: region,
+                        sources: [],
+                        targets: []
                     };
-
-                    if(obj.dests[0] === undefined) {
-                        obj.dests = [];
-                    }
-
-                    return obj;
                 })
                 .value();
 
-            return formatted_data; // this is passed into updateView as 'data'
+            var links = _(data)
+                .chain()
+                .filter(function(o) { return o.from && o.to; })
+                .map(function(o) {
+                    var source = _(nodes).findWhere({name: o.from});
+                    var target = _(nodes).findWhere({name: o.to});
+
+                    source.targets.push(target);
+                    target.sources.push(source);
+
+                    return {
+                        source: source,
+                        target: target
+                    };
+                })
+                .value();
+
+            // this is passed into updateView as 'data'
+            return {
+                nodes: nodes,
+                links: links,
+                axes: categories.length
+            };
         },
         updateView: function(viz, data) {
             var that = this;
@@ -65,260 +106,137 @@ define(function(require, exports, module) {
             // clearing all prior junk from the view (eg. 'waiting for data...')
             that.$el.html("");
 
-            var width = that.$el.width(), //960,
-                height = 900, //850,
+            var nodes = data.nodes;
+            var links = data.links;
+            var axes  = data.axes;
+
+            var width = 960,
+                height = 500,
                 innerRadius = 40,
-                outerRadius = 640,
-                majorAngle = 2 * Math.PI / 3,
-                minorAngle = 1 * Math.PI / 12;
+                outerRadius = 240;
 
-            var angle = d3.scale.ordinal()
-                .domain(["source", "source-target", "target-source", "target"])
-                .range([0, majorAngle - minorAngle, majorAngle + minorAngle, 2 * majorAngle]);
+            var angle = d3.scale.ordinal().domain(d3.range(axes + 1)).rangePoints([0, 2 * Math.PI]),
+                radius = d3.scale.linear().range([innerRadius, outerRadius]),
+                color = d3.scale.category10().domain(d3.range(20));
 
-            var radius = d3.scale.linear()
-                .range([innerRadius, outerRadius]);
+            var formatNumber = d3.format(",d"),
+                formatCommaNumber = d3.format("0,000"),
+                defaultInfo;
 
-            var color = d3.scale.category10();
+            var links_name = that.settings.get("links_name");
+            var nodes_name = that.settings.get("nodes_name");
+
+            // Initialize the info display.
+            var info = d3.select("#info")
+                .text(defaultInfo = "Showing " + formatNumber(links.length) + " " + links_name + " among " + formatNumber(nodes.length) + " " + nodes_name + ".");
+
 
             var svg = d3.select(that.el).append("svg")
                 .attr("width", width)
                 .attr("height", height)
             .append("g")
-                .attr("transform", "translate(" + outerRadius * .40 + "," + outerRadius * .57 + ")");
+                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-            // Load the data and display the plot!
-            var nodes = data;
-
-            var nodesByName = {},
-                links = [],
-                formatNumber = d3.format(",d"),
-                defaultInfo;
-
-            // Construct an index by node name.
-            nodes.forEach(function(d) {
-                d.connectors = [];
-                nodesByName[d.src] = d;
-            });
-
-            // Convert the import lists into links with sources and targets.
-            nodes.forEach(function(source) {
-                source.dests.forEach(function(targetName) {
-                    var target = nodesByName[targetName];
-                    if (!source.source) source.connectors.push(source.source = {node: source, degree: 0});
-                    if (!target.target) target.connectors.push(target.target = {node: target, degree: 0});
-                    links.push({source: source.source, target: target.target});
-                });
-            });
-
-            // Determine the type of each node, based on incoming and outgoing links.
-            nodes.forEach(function(node) {
-                if(node.source && node.target) {
-                    node.type = node.source.type = "target-source";
-                    node.target.type = "source-target";
-                }
-                else if(node.source) {
-                    node.type = node.source.type = "source";
-                }
-                else if(node.target) {
-                    node.type = node.target.type = "target";
-                }
-                else {
-                    node.connectors = [{node: node}];
-                    node.type = "source";
-                }
-            });
-
-            // Initialize the info display.
-            var info = d3.select("#info")
-                .text(defaultInfo = "Showing " + formatNumber(links.length) + " dependencies among " + formatNumber(nodes.length) + " classes.");
-
-            // Normally, Hive Plots sort nodes by degree along each axis. However, since
-            // this example visualizes a package hierarchy, we get more interesting
-            // results if we group nodes by package. We don't need to sort explicitly
-            // because the data file is already sorted by class name.
-
-            // Nest nodes by type, for computing the rank.
-            var nodesByType = d3.nest()
-                .key(function(d) { return d.type; })
-                .sortKeys(d3.ascending)
-                .entries(nodes);
-
-            // Duplicate the target-source axis as source-target.
-            nodesByType.push({key: "source-target", values: nodesByType[2].values});
-
-            // Compute the rank for each type, with padding between packages.
-            nodesByType.forEach(function(type) {
-                var lastName = type.values[0].category, count = 0;
-                type.values.forEach(function(d, i) {
-                if (d.category != lastName) lastName = d.category, count += 2;
-                d.index = count++;
-                });
-                type.count = count - 1;
-            });
-
-            // Set the radius domain.
-            radius.domain(d3.extent(nodes, function(d) { return d.index; }));
-
-            // Draw the axes.
             svg.selectAll(".axis")
-                .data(nodesByType)
-                .enter().append("line")
+                .data(d3.range(axes))
+            .enter().append("line")
                 .attr("class", "axis")
-                .attr("transform", function(d) { return "rotate(" + degrees(angle(d.key)) + ")"; })
-                .attr("x1", radius(-2))
-                .attr("x2", function(d) { return radius(d.count + 2); });
+                .attr("transform", function(d) { return "rotate(" + degrees(angle(d)) + ")"; })
+                .attr("x1", radius.range()[0])
+                .attr("x2", radius.range()[1]);
 
-            // Draw the links.
-            svg.append("g")
-                .attr("class", "links")
-                .selectAll(".link")
+            svg.selectAll(".link")
                 .data(links)
-                .enter().append("path")
+            .enter().append("path")
                 .attr("class", "link")
-                .attr("d", link()
-                .angle(function(d) { return angle(d.type); })
-                .radius(function(d) { return radius(d.node.index); }))
+                .attr("d", d3.hive.link()
+                .angle(function(d) { return angle(d.x); })
+                .radius(function(d) { return radius(d.y); }))
+                .style("stroke", function(d) { return color(d.source.x); })
                 .on("mouseover", linkMouseover)
-                .on("mouseout", mouseout);
+                .on("mouseout", mouseout)
+                .append("svg:title")
+                .text(function(d) {
+                    return d.source.name + " → " + d.target.name;
+                });
 
-            // Draw the nodes. Note that each node can have up to two connectors,
-            // representing the source (outgoing) and target (incoming) links.
-            svg.append("g")
-                .attr("class", "nodes")
-                .selectAll(".node")
+            var node = svg.selectAll(".node")
                 .data(nodes)
-                .enter().append("g")
+            .enter().append("g")
+                .attr("class", "node");
+
+            node.append("circle")
                 .attr("class", "node")
-                .style("fill", function(d) { return color(d.category); })
-                .selectAll("circle")
-                .data(function(d) { return d.connectors; })
-                .enter().append("circle")
-                .attr("transform", function(d) { return "rotate(" + degrees(angle(d.type)) + ")"; })
-                .attr("cx", function(d) { return radius(d.node.index); })
-                .attr("r", 4)
+                .attr("transform", function(d) { return "rotate(" + degrees(angle(d.x)) + ")"; })
+                .attr("cx", function(d) { return radius(d.y); })
+                .attr("r", 5)
+                .style("fill", function(d) { return color(d.x); })
                 .on("mouseover", nodeMouseover)
                 .on("mouseout", mouseout)
-                .on("click", nodeClick);
+                .append("svg:title")
+                .text(function(d) {
+                    var info = [];
+                    _(d.targets).each(function(o) {
+                        info.push(d.name + " → " + o.name);
+                    });
 
-            function nodeClick(d) {
-                var e = {
-                    "src": d.node.src,
-                    "dests": d.node.dests,
-                    "category": d.node.category
-                };
-                trigger("click", e);
+                    _(d.sources).each(function(o) {
+                        info.push(o.name + " → " + d.name);
+                    });
+
+                    return info.join("\n");
+                });
+
+            node.append("text")
+                .attr("transform", function(d) { return "rotate(" + degrees(angle(d.x)) + ")"; })
+                .attr("dx", function(d) { return radius(d.y); })
+                .attr("dy", ".35em")
+                .text(function(d) { return d.name });
+
+            function degrees(radians) {
+                return radians / Math.PI * 180 - 90;
             }
 
             // Highlight the link and connected nodes on mouseover.
             function linkMouseover(d) {
-                svg.selectAll(".link").classed("active", function(p) { return p === d; });
-                svg.selectAll(".node circle").classed("active", function(p) { return p === d.source || p === d.target; });
-                info.text(d.source.node.src + " → " + d.target.node.src);
+                svg.selectAll(".link")
+                    .classed("active", function(p) {
+                        return p === d;
+                    });
+                svg.selectAll(".node")
+                    .classed("active", function(p) {
+                        return p === d.source || p === d.target;
+                    });
+                info.text(d.source.name + " → " + d.target.name);
             }
 
             // Highlight the node and connected links on mouseover.
             function nodeMouseover(d) {
-                svg.selectAll(".link").classed("active", function(p) { return p.source === d || p.target === d; });
-                svg.selectAll(".node circle").classed("active", function(p) { return p === d.source || p === d.target; });
+                svg.selectAll(".link")
+                    .classed("active", function(p) {
+                        return p.source === d;
+                    })
+                    .classed("hidden", function(p) {
+                        return p.source !== d;
+                    });
+                svg.selectAll(".node")
+                    .classed("active", function(p) {
+                        return p === d || d.targets.indexOf(p) > -1;
+                    })
+                    .classed("hidden", function(p) {
+                        return !(p == d || d.targets.indexOf(p) > -1);
+                    });
+                svg.selectAll(".axis").classed("hidden", true);
                 d3.select(this).classed("active", true);
-                info.text(d.node.src);
+                info.text("Country: " + d.name + ", Region: " + d.region + ", Population: " + formatCommaNumber(d.population));
             }
 
             // Clear any highlighted nodes or links.
             function mouseout() {
                 svg.selectAll(".active").classed("active", false);
+                svg.selectAll(".hidden").classed("hidden", false);
                 info.text(defaultInfo);
             }
-
-            // A shape generator for Hive links, based on a source and a target.
-            // The source and target are defined in polar coordinates (angle and radius).
-            // Ratio links can also be drawn by using a startRadius and endRadius.
-            // This class is modeled after d3.svg.chord.
-            function link() {
-            var source = function(d) { return d.source; },
-                target = function(d) { return d.target; },
-                angle = function(d) { return d.angle; },
-                startRadius = function(d) { return d.radius; },
-                endRadius = startRadius,
-                arcOffset = -Math.PI / 2;
-
-            function link(d, i) {
-                var s = node(source, this, d, i),
-                    t = node(target, this, d, i),
-                    x;
-                if (t.a < s.a) x = t, t = s, s = x;
-                if (t.a - s.a > Math.PI) s.a += 2 * Math.PI;
-                var a1 = s.a + (t.a - s.a) / 3,
-                    a2 = t.a - (t.a - s.a) / 3;
-                return s.r0 - s.r1 || t.r0 - t.r1
-                    ? "M" + Math.cos(s.a) * s.r0 + "," + Math.sin(s.a) * s.r0
-                    + "L" + Math.cos(s.a) * s.r1 + "," + Math.sin(s.a) * s.r1
-                    + "C" + Math.cos(a1) * s.r1 + "," + Math.sin(a1) * s.r1
-                    + " " + Math.cos(a2) * t.r1 + "," + Math.sin(a2) * t.r1
-                    + " " + Math.cos(t.a) * t.r1 + "," + Math.sin(t.a) * t.r1
-                    + "L" + Math.cos(t.a) * t.r0 + "," + Math.sin(t.a) * t.r0
-                    + "C" + Math.cos(a2) * t.r0 + "," + Math.sin(a2) * t.r0
-                    + " " + Math.cos(a1) * s.r0 + "," + Math.sin(a1) * s.r0
-                    + " " + Math.cos(s.a) * s.r0 + "," + Math.sin(s.a) * s.r0
-                    : "M" + Math.cos(s.a) * s.r0 + "," + Math.sin(s.a) * s.r0
-                    + "C" + Math.cos(a1) * s.r1 + "," + Math.sin(a1) * s.r1
-                    + " " + Math.cos(a2) * t.r1 + "," + Math.sin(a2) * t.r1
-                    + " " + Math.cos(t.a) * t.r1 + "," + Math.sin(t.a) * t.r1;
-            }
-
-            function node(method, thiz, d, i) {
-                var node = method.call(thiz, d, i),
-                    a = +(typeof angle === "function" ? angle.call(thiz, node, i) : angle) + arcOffset,
-                    r0 = +(typeof startRadius === "function" ? startRadius.call(thiz, node, i) : startRadius),
-                    r1 = (startRadius === endRadius ? r0 : +(typeof endRadius === "function" ? endRadius.call(thiz, node, i) : endRadius));
-                return {r0: r0, r1: r1, a: a};
-            }
-
-            link.source = function(_) {
-                if (!arguments.length) return source;
-                source = _;
-                return link;
-            };
-
-            link.target = function(_) {
-                if (!arguments.length) return target;
-                target = _;
-                return link;
-            };
-
-            link.angle = function(_) {
-                if (!arguments.length) return angle;
-                angle = _;
-                return link;
-            };
-
-            link.radius = function(_) {
-                if (!arguments.length) return startRadius;
-                startRadius = endRadius = _;
-                return link;
-            };
-
-            link.startRadius = function(_) {
-                if (!arguments.length) return startRadius;
-                startRadius = _;
-                return link;
-            };
-
-            link.endRadius = function(_) {
-                if (!arguments.length) return endRadius;
-                endRadius = _;
-                return link;
-            };
-
-            return link;
-            }
-
-            function degrees(radians) {
-            return radians / Math.PI * 180 - 90;
-            }
-
-
         }
     });
     return HivePlot;
