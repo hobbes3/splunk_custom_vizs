e5d2fc1e003ca65901d0ab44d426ef068a0319b0
hobbes3 <hobbes3@gmail.com>
2015-08-13 21:32:52 -0700

    Added a description page for Voronoi. Unhardcoded everything. Made it so hovering doesn't hide the voronoi regions.

--- appserver/static/components/voronoi/voronoi.js
+++ appserver/static/components/voronoi/voronoi.js
@@ -15,9 +15,14 @@ define(function(require, exports, module) {
         options: {
             managerid: null,
             data: "preview",
-            nameField: null,
-            valueField: 'count',
-            categoryField: null
+            src_label_field: "src",
+            src_lat_field: "src_lat",
+            src_lon_field: "src_lon",
+            dst_label_field: "dst",
+            dst_lat_field: "dst_lat",
+            dst_lon_field: "dst_lon",
+            count_field: "count",
+            height: 600
         },
 
         output_mode: "json",
@@ -32,11 +37,17 @@ define(function(require, exports, module) {
             // in the case that any options are changed, it will dynamically update
             // without having to refresh. copy the following line for whichever field
             // you'd like dynamic updating on
-            this.settings.on("change:foo", this.render, this);
+            this.settings.on("change:src_label_field", this.render, this);
+            this.settings.on("change:src_lat_field", this.render, this);
+            this.settings.on("change:src_lon_field", this.render, this);
+            this.settings.on("change:dst_label", this.render, this);
+            this.settings.on("change:dst_lat_field", this.render, this);
+            this.settings.on("change:dst_lon_field", this.render, this);
+            this.settings.on("change:count_field", this.render, this);
 
             // Set up resize callback. The first argument is a this
             // pointer which gets passed into the callback event
-            //$(window).resize(this, _.debounce(this._handleResize, 20));
+            $(window).resize(this, _.debounce(this._handleResize, 20));
         },
 
         _handleResize: function(e) {
@@ -54,17 +65,25 @@ define(function(require, exports, module) {
 
         // making the data look how we want it to for updateView to do its job
         formatData: function(data) {
-            // getting settings
             return data;
         },
 
         updateView: function(viz, data) {
             var that = this;
 
-            that.$el.html("");
+            var src_label_field = that.settings.get("src_label_field");
+            var src_lat_field   = that.settings.get("src_lat_field");
+            var src_lon_field   = that.settings.get("src_lon_field");
+            var dst_label_field = that.settings.get("dst_label_field");
+            var dst_lat_field   = that.settings.get("dst_lat_field");
+            var dst_lon_field   = that.settings.get("dst_lon_field");
 
-            var width = 960,
-                height = 500;
+            var count_field = that.settings.get("count_field");
+
+            var height = that.settings.get("height");
+            var width = that.$el.width();
+
+            that.$el.html("");
 
             var projection = d3.geo.albers()
                 .translate([width / 2, height / 2])
@@ -89,49 +108,49 @@ define(function(require, exports, module) {
             function ready(error, us) {
             if (error) throw error;
 
-            var airportById = d3.map(),
+            var get_points_by_id = d3.map(),
                 positions = [];
 
-            var from = _(data).chain().groupBy("from").each(function(v, k, o) { o[k] = v[0] }).value();
-            var to   = _(data).chain().groupBy("to")  .each(function(v, k, o) { o[k] = v[0] }).value();
+            var src = _(data).chain().groupBy(src_label_field).each(function(v, k, o) { o[k] = v[0] }).value();
+            var dst = _(data).chain().groupBy(dst_label_field).each(function(v, k, o) { o[k] = v[0] }).value();
 
-            var uniques = _(from).extend(to);
+            var uniques = _(src).extend(dst);
 
-            var airports = _(uniques).map(function(v, k) {
+            var points = _(uniques).map(function(v, k) {
                 var o = {};
 
                 o.id = k;
 
                 if(v.from === k) {
-                    o.latitude  = v.from_lat;
-                    o.longitude = v.from_lon;
+                    o.lat = v[src_lat_field];
+                    o.lon = v[src_lon_field];
                 }
                 else {
-                    o.latitude  = v.to_lat;
-                    o.longitude = v.to_lon;
+                    o.lat = v[dst_lat_field];
+                    o.lon = v[dst_lon_field];
                 }
 
                 return o;
             });
 
-            airports.forEach(function(d) {
-                airportById.set(d.id, d);
+            points.forEach(function(d) {
+                get_points_by_id.set(d.id, d);
                 d.outgoing = [];
                 d.incoming = [];
             });
 
-            data.forEach(function(flight) {
-                var source = airportById.get(flight.from),
-                    target = airportById.get(flight.to),
+            data.forEach(function(connection) {
+                var source = get_points_by_id.get(connection[src_label_field]),
+                    target = get_points_by_id.get(connection[dst_label_field]),
                     link = {source: source, target: target};
                 source.outgoing.push(link);
                 target.incoming.push(link);
             });
 
-            airports = airports.filter(function(d) {
+            points = points.filter(function(d) {
                 if (d.count = Math.max(d.incoming.length, d.outgoing.length)) {
-                d[0] = +d.longitude;
-                d[1] = +d.latitude;
+                d[0] = +d.lon;
+                d[1] = +d.lat;
                 var position = projection(d);
                 d.x = position[0];
                 d.y = position[1];
@@ -139,40 +158,40 @@ define(function(require, exports, module) {
                 }
             });
 
-            voronoi(airports)
+            voronoi(points)
                 .forEach(function(d) { d.point.cell = d; });
 
             svg.append("path")
                 .datum(topojson.feature(us, us.objects.land))
-                .attr("class", "states")
+                .attr("class", "regions")
                 .attr("d", path);
 
             svg.append("path")
                 .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-                .attr("class", "state-borders")
+                .attr("class", "region-borders")
                 .attr("d", path);
 
-            var airport = svg.append("g")
-                .attr("class", "airports")
+            var point = svg.append("g")
+                .attr("class", "points")
                 .selectAll("g")
-                .data(airports.sort(function(a, b) { return b.count - a.count; }))
+                .data(points.sort(function(a, b) { return b[count_field] - a[count_field]; }))
                 .enter().append("g")
-                .attr("class", "airport");
+                .attr("class", "point");
 
-            airport.append("path")
-                .attr("class", "airport-cell")
+            point.append("path")
+                .attr("class", "point-cell")
                 .attr("d", function(d) { return d.cell.length ? "M" + d.cell.join("L") + "Z" : null; });
 
-            airport.append("g")
-                .attr("class", "airport-arcs")
+            point.append("g")
+                .attr("class", "point-arcs")
                 .selectAll("path")
                 .data(function(d) { return d.outgoing; })
                 .enter().append("path")
                 .attr("d", function(d) { return path({type: "LineString", coordinates: [d.source, d.target]}); });
 
-            airport.append("circle")
+            point.append("circle")
                 .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-                .attr("r", function(d, i) { return Math.sqrt(d.count); });
+                .attr("r", function(d, i) { return Math.sqrt(d[count_field]); });
 
             }
         }
---
+++ appserver/static/description/viz_voronoi.md
@@ -0,0 +1,91 @@
+<h4>Options</h4>
+<table class="table table-striped table-bordered">
+    <tbody>
+    <tr>
+        <td>
+        <b>Name</b>
+        </td>
+        <td>
+        <b>Type</b>
+        </td>
+        <td>
+        <b>Default</b>
+        </td>
+        <td>
+        <b>Description</b>
+        </td>
+    </tr>
+    <tr>
+        <td>
+        <code>managerid</code>
+        </td>
+        <td>string</td>
+        <td>null</td>
+        <td>The search manager bound to the chart.</td>
+    </tr>
+    <tr>
+        <td>
+        <code>height</code>
+        </td>
+        <td>number</td>
+        <td>600</td>
+        <td>The height of the panel.</td>
+    </tr>
+    <tr>
+        <td>
+        <code>src_label_field</code>
+        </td>
+        <td>string</td>
+        <td>"src"</td>
+        <td>The name of the source field.</td>
+    </tr>
+    <tr>
+        <td>
+        <code>src_lat_field</code>
+        </td>
+        <td>string</td>
+        <td>"src_lat"</td>
+        <td>The name of the latitude for the source field.</td>
+    </tr>
+    <tr>
+        <td>
+        <code>src_lon_field</code>
+        </td>
+        <td>string</td>
+        <td>"src_lon"</td>
+        <td>The name of the longitude for the source field.</td>
+    </tr>
+    <tr>
+        <td>
+        <code>dst_label_field</code>
+        </td>
+        <td>string</td>
+        <td>"dst"</td>
+        <td>The name of the source field.</td>
+    </tr>
+    <tr>
+        <td>
+        <code>dst_lat_field</code>
+        </td>
+        <td>string</td>
+        <td>"dst_lat"</td>
+        <td>The name of the latitude for the destination field.</td>
+    </tr>
+    <tr>
+        <td>
+        <code>dst_lon_field</code>
+        </td>
+        <td>string</td>
+        <td>"dst_lon"</td>
+        <td>The name of the longitude for the desination field.</td>
+    </tr>
+    <tr>
+        <td>
+        <code>count_field</code>
+        </td>
+        <td>string</td>
+        <td>"count"</td>
+        <td>The name of the count field.</td>
+    </tr>
+    </tbody>
+</table>
