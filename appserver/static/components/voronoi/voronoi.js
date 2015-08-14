a1c68d2b2a3c942407c7d1815085ddc48b7d3977
hobbes3 <hobbes3@gmail.com>
2015-08-13 23:22:27 -0700

    Couldn't figure out how to get <title> tooltip working.

--- appserver/static/components/voronoi/voronoi.js
+++ appserver/static/components/voronoi/voronoi.js
@@ -106,93 +106,92 @@ define(function(require, exports, module) {
                 .await(ready);
 
             function ready(error, us) {
-            if (error) throw error;
+                if (error) throw error;
 
-            var get_points_by_id = d3.map(),
-                positions = [];
+                var get_points_by_id = d3.map(),
+                    positions = [];
 
-            var src = _(data).chain().groupBy(src_label_field).each(function(v, k, o) { o[k] = v[0] }).value();
-            var dst = _(data).chain().groupBy(dst_label_field).each(function(v, k, o) { o[k] = v[0] }).value();
+                var src = _(data).chain().groupBy(src_label_field).each(function(v, k, o) { o[k] = v[0] }).value();
+                var dst = _(data).chain().groupBy(dst_label_field).each(function(v, k, o) { o[k] = v[0] }).value();
 
-            var uniques = _(src).extend(dst);
+                var uniques = _(src).extend(dst);
 
-            var points = _(uniques).map(function(v, k) {
-                var o = {};
+                var points = _(uniques).map(function(v, k) {
+                    var o = {};
 
-                o.id = k;
+                    o.id = k;
 
-                if(v.from === k) {
-                    o.lat = v[src_lat_field];
-                    o.lon = v[src_lon_field];
-                }
-                else {
-                    o.lat = v[dst_lat_field];
-                    o.lon = v[dst_lon_field];
-                }
+                    if(v.from === k) {
+                        o.lat = v[src_lat_field];
+                        o.lon = v[src_lon_field];
+                    }
+                    else {
+                        o.lat = v[dst_lat_field];
+                        o.lon = v[dst_lon_field];
+                    }
 
-                return o;
-            });
+                    return o;
+                });
 
-            points.forEach(function(d) {
-                get_points_by_id.set(d.id, d);
-                d.outgoing = [];
-                d.incoming = [];
-            });
+                points.forEach(function(d) {
+                    get_points_by_id.set(d.id, d);
+                    d.outgoing = [];
+                    d.incoming = [];
+                });
 
-            data.forEach(function(connection) {
-                var source = get_points_by_id.get(connection[src_label_field]),
-                    target = get_points_by_id.get(connection[dst_label_field]),
-                    link = {source: source, target: target};
-                source.outgoing.push(link);
-                target.incoming.push(link);
-            });
+                data.forEach(function(connection) {
+                    var source = get_points_by_id.get(connection[src_label_field]),
+                        target = get_points_by_id.get(connection[dst_label_field]),
+                        link = {source: source, target: target};
+                    source.outgoing.push(link);
+                    target.incoming.push(link);
+                });
 
-            points = points.filter(function(d) {
-                if (d.count = Math.max(d.incoming.length, d.outgoing.length)) {
-                d[0] = +d.lon;
-                d[1] = +d.lat;
-                var position = projection(d);
-                d.x = position[0];
-                d.y = position[1];
-                return true;
-                }
-            });
+                points = points.filter(function(d) {
+                    if (d.count = Math.max(d.incoming.length, d.outgoing.length)) {
+                    d[0] = +d.lon;
+                    d[1] = +d.lat;
+                    var position = projection(d);
+                    d.x = position[0];
+                    d.y = position[1];
+                    return true;
+                    }
+                });
 
-            voronoi(points)
-                .forEach(function(d) { d.point.cell = d; });
+                voronoi(points)
+                    .forEach(function(d) { d.point.cell = d; });
 
-            svg.append("path")
-                .datum(topojson.feature(us, us.objects.land))
-                .attr("class", "regions")
-                .attr("d", path);
+                svg.append("path")
+                    .datum(topojson.feature(us, us.objects.land))
+                    .attr("class", "regions")
+                    .attr("d", path);
 
-            svg.append("path")
-                .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-                .attr("class", "region-borders")
-                .attr("d", path);
+                svg.append("path")
+                    .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+                    .attr("class", "region-borders")
+                    .attr("d", path);
 
-            var point = svg.append("g")
-                .attr("class", "points")
-                .selectAll("g")
-                .data(points.sort(function(a, b) { return b[count_field] - a[count_field]; }))
-                .enter().append("g")
-                .attr("class", "point");
+                var point = svg.append("g")
+                    .attr("class", "points")
+                    .selectAll("g")
+                    .data(points.sort(function(a, b) { return b[count_field] - a[count_field]; }))
+                    .enter().append("g")
+                    .attr("class", "point");
 
-            point.append("path")
-                .attr("class", "point-cell")
-                .attr("d", function(d) { return d.cell.length ? "M" + d.cell.join("L") + "Z" : null; });
+                point.append("path")
+                    .attr("class", "point-cell")
+                    .attr("d", function(d) { return d.cell.length ? "M" + d.cell.join("L") + "Z" : null; });
 
-            point.append("g")
-                .attr("class", "point-arcs")
-                .selectAll("path")
-                .data(function(d) { return d.outgoing; })
-                .enter().append("path")
-                .attr("d", function(d) { return path({type: "LineString", coordinates: [d.source, d.target]}); });
+                point.append("g")
+                    .attr("class", "point-arcs")
+                    .selectAll("path")
+                    .data(function(d) { return d.outgoing; })
+                    .enter().append("path")
+                    .attr("d", function(d) { return path({type: "LineString", coordinates: [d.source, d.target]}); });
 
-            point.append("circle")
-                .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-                .attr("r", function(d, i) { return Math.sqrt(d[count_field]); });
-
+                point.append("circle")
+                    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+                    .attr("r", function(d, i) { return Math.sqrt(d[count_field]); });
             }
         }
     });
