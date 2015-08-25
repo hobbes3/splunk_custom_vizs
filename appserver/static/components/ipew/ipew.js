646d92120e107364d7a274dce2c6e4fe7e7cbf85
hobbes3 <hobbes3@gmail.com>
2015-08-25 01:02:13 -0700

    Un-hardcoded the options for IPew. Added other sounds effects. Added the description for all the options. Fixed the real-time event unique attacks issue.

--- appserver/static/components/ipew/ipew.js
+++ appserver/static/components/ipew/ipew.js
@@ -7,19 +7,37 @@ define(function(require, exports, module) {
     var topojson = require("../d3/topojson");
     var Datamap = require("../d3/datamaps.world");
     var SimpleSplunkView = require("splunkjs/mvc/simplesplunkview");
+    require("css!./ipew.css");
 
     var IPew = SimpleSplunkView.extend({
         className: "splunk-ipew",
         options: {
             "managerid": null,
-            "data": "preview"
+            "data": "preview",
+            "height": 800,
+            "src_lat_field": "src_lat",
+            "src_lon_field": "src_lon",
+            "dst_lat_field": "dst_lat",
+            "dst_lon_field": "dst_lon",
+            "type_field": "type",
+            "id_field": "id",
+            "sound_filename": "/static/app/custom_vizs/components/ipew/Blaster-Solo.wav",
+            "stroke_color": "green",
+            "stroke_width": 2,
+            "queue": 7,
+            "limit": 2
         },
         output_mode: "json",
         initialize: function() {
             SimpleSplunkView.prototype.initialize.apply(this, arguments);
             // In the case that any options are changed, it will dynamically update without having to refresh
             // Copy the following line for whichever field you'd like dynamic updating on
-            this.settings.on("change:count_field", this.render, this);
+            this.settings.on("change:src_lat_field", this.render, this);
+            this.settings.on("change:src_lon_field", this.render, this);
+            this.settings.on("change:dst_lat_field", this.render, this);
+            this.settings.on("change:dst_lon_field", this.render, this);
+            this.settings.on("change:type_field",    this.render, this);
+            this.settings.on("change:id_field",      this.render, this);
         },
         createView: function() {
             var that = this;
@@ -27,46 +45,13 @@ define(function(require, exports, module) {
             // clearing all prior junk from the view (eg. 'waiting for data...')
             that.$el.html("");
 
-            function FixedQueue(size, initialValues) {
-                initialValues = initialValues || [];
-                var queue = Array.apply(null, initialValues);
-                queue.fixedSize = size;
-                queue.push = FixedQueue.push;
-                queue.splice = FixedQueue.splice;
-                queue.unshift = FixedQueue.unshift;
-                FixedQueue.trimTail.call(queue);
-                return queue;
-            }
-
-            FixedQueue.trimHead = function() {
-                if(this.length <= this.fixedSize) {
-                    return;
-                }
-                Array.prototype.splice.call(this, 0, this.length - this.fixedSize);
-            };
-            FixedQueue.trimTail = function() {
-                if (this.length <= this.fixedSize) {
-                    return;
-                }
-                Array.prototype.splice.call(this, this.fixedSize, this.length - this.fixedSize);
-            };
-            FixedQueue.wrapMethod = function(methodName, trimMethod) {
-                var wrapper = function() {
-                    var method = Array.prototype[ methodName ];
-                    var result = method.apply( this, arguments );
-                    trimMethod.call( this );
-                    return result;
-                };
-                return wrapper;
-            };
-            FixedQueue.push = FixedQueue.wrapMethod("push", FixedQueue.trimHead);
-            FixedQueue.splice = FixedQueue.wrapMethod("splice", FixedQueue.trimTail);
-            FixedQueue.unshift = FixedQueue.wrapMethod("unshift", FixedQueue.trimTail);
+            var height = that.settings.get("height");
+            var sound_filename = that.settings.get("sound_filename");
 
             var map = new Datamap({
                 scope: 'world',
                 element: that.el,
-                height: 900,
+                height: height,
                 projection: 'winkel3',
                 // change the projection to something else only if you have absolutely no cartographic sense
                 fills: { defaultFill: 'black', },
@@ -87,10 +72,14 @@ define(function(require, exports, module) {
                 },
             });
 
-            $("body").append('<audio id="pew_sound" src="/static/app/custom_vizs/components/ipew/Blaster-Solo.wav" preload="auto">');
+            if(sound_filename) {
+                $("body").append('<audio id="pew_sound" src="' + sound_filename + '" preload="auto">');
+            }
 
-            that.hits = FixedQueue(7, [  ]);
-            that.boom = FixedQueue(7, [  ]);
+            that.last_event = "";
+
+            that.hits = [];
+            that.boom = [];
 
             return map;
         },
@@ -101,43 +90,78 @@ define(function(require, exports, module) {
         updateView: function(map, data) {
             var that = this;
 
-            document.getElementById("pew_sound").load();
-            document.getElementById("pew_sound").play();
+            clearTimeout(that.timer);
 
-            var srclat = data[0].src_lat;
-            var srclong = data[0].src_lon;
-            var dstlat = data[0].dst_lat;
-            var dstlong = data[0].dst_lon;
+            var src_lat_field = that.settings.get("src_lat_field");
+            var src_lon_field = that.settings.get("src_lon_field");
+            var dst_lat_field = that.settings.get("dst_lat_field");
+            var dst_lon_field = that.settings.get("dst_lon_field");
+            var type_field    = that.settings.get("type_field");
+            var id_field      = that.settings.get("id_field");
 
-            var strokeColor = 'green';
+            var sound_filename = that.settings.get("sound_filename");
+            var stroke_color = that.settings.get("stroke_color");
+            var stroke_width = that.settings.get("stroke_width");
+            var queue = that.settings.get("queue");
+            var limit = that.settings.get("limit");
 
-            that.hits.push({
-                origin: {
-                    latitude: +srclat,
-                    longitude: +srclong
-                },
-                destination: {
-                    latitude: +dstlat,
-                    longitude: +dstlong
+            if(sound_filename) {
+                document.getElementById("pew_sound").load();
+                document.getElementById("pew_sound").play();
+            }
+
+            var i;
+            for(i = 0; i < limit; i++) {
+                var src_lat = data[i][src_lat_field];
+                var src_lon = data[i][src_lon_field];
+                var dst_lat = data[i][dst_lat_field];
+                var dst_lon = data[i][dst_lon_field];
+                var type    = data[i][type_field];
+                var id      = data[i][id_field];
+
+                var raw_event = data[i][id_field];
+
+                if(raw_event === that.last_event) {
+                    break;
                 }
-            });
-            map.arc(that.hits, {
-                strokeWidth: 2,
-                strokeColor: strokeColor
-            });
-            // add boom to the bubbles queue
-            that.boom.push({
-                radius: 7,
-                latitude: +dstlat,
-                longitude: +dstlong,
-                fillOpacity: 0.5,
-                attk: data[0].type
-            });
-            map.bubbles(that.boom, {
-                popupTemplate: function(geo, data) {
-                    return '<div class="hoverinfo">' + data.attk + '</div>';
+
+                that.hits.push({
+                    origin: {
+                        latitude: +src_lat,
+                        longitude: +src_lon
+                    },
+                    destination: {
+                        latitude: +dst_lat,
+                        longitude: +dst_lon
+                    }
+                });
+                map.arc(that.hits, {
+                    strokeWidth: stroke_width,
+                    strokeColor: stroke_color
+                });
+                // add boom to the bubbles queue
+                that.boom.push({
+                    radius: 7,
+                    latitude: +dst_lat,
+                    longitude: +dst_lon,
+                    fillOpacity: 0.5,
+                    attk: data[i][type_field]
+                });
+                map.bubbles(that.boom, {
+                    popupTemplate: function(geo, data) {
+                        return '<div class="hoverinfo">' + data.attk + '</div>';
+                    }
+                });
+
+                var length = that.hits.length;
+
+                if(length > queue) {
+                    that.hits.shift();
+                    that.boom.shift();
                 }
-            });
+            }
+
+            that.last_event = data[0][id_field];
         }
     });
     return IPew;
--- appserver/static/description/viz_ipew.md
+++ appserver/static/description/viz_ipew.md
@@ -1,3 +1,7 @@
+<h4>Requirements</h4>
+<p>
+    Install <a href="https://splunkbase.splunk.com/app/1924/#/overview" target="_blank">Splunk Eventgen app</a> to see a live demo of this dashboard. Rename the Eventgen app to <code>SA-eventgen</code> and restart Splunk. Note that the <code>eventgen.conf</code> from this app will generate one sample "attack" event to <code>index=main sourcetype=ipew_sample</code> every second.
+</p>
 <h4>Options</h4>
 <table class="table table-striped table-bordered">
     <tbody>
@@ -22,6 +26,102 @@
             <td>string</td>
             <td>null</td>
             <td>The search manager bound to the chart.</td>
+        </tr>
+        <tr>
+            <td>
+            <code>height</code>
+            </td>
+            <td>number</td>
+            <td>800</td>
+            <td>The height of the panel.</td>
+        </tr>
+        <tr>
+            <td>
+            <code>src_lat_field</code>
+            </td>
+            <td>string</td>
+            <td>"src_lat"</td>
+            <td>The name of the source's latitude field.</td>
+        </tr>
+        <tr>
+            <td>
+            <code>src_lon_field</code>
+            </td>
+            <td>string</td>
+            <td>"src_lon"</td>
+            <td>The name of the source's longitude field.</td>
+        </tr>
+        <tr>
+            <td>
+            <code>dst_lat_field</code>
+            </td>
+            <td>string</td>
+            <td>"dst_lat"</td>
+            <td>The name of the destination's latitude field.</td>
+        </tr>
+        <tr>
+            <td>
+            <code>dst_lon_field</code>
+            </td>
+            <td>string</td>
+            <td>"dst_lon"</td>
+            <td>The name of the destination's longitude field.</td>
+        </tr>
+        <tr>
+            <td>
+            <code>type_field</code>
+            </td>
+            <td>string</td>
+            <td>"type"</td>
+            <td>The name of the type field. This is what is shown in the hover popup at the destination's circle.</td>
+        </tr>
+        <tr>
+            <td>
+            <code>id_field</code>
+            </td>
+            <td>string</td>
+            <td>"id"</td>
+            <td>The name of the id field. The id should be a unique value for each event. One way to achieve this is with <code>md5(_raw)</code> in the search query.</td>
+        </tr>
+        <tr>
+            <td>
+            <code>sound_field</code>
+            </td>
+            <td>string</td>
+            <td>"/static/app/custom_vizs/components/ipew/Blaster-Solo.wav"</td>
+            <td>The relative web URL to the sound effect. Setting the value to <code>null</code> will disable the sound effect. This app provides the following files: <code>B5-interceptor1.wav</code>, <code>Blaster-Solo.wav</code>, <code>pew.mp3</code>, <code>shot_sound.mp3</code>, <code>tng_torpedo_clean.mp3</code>, and <code>WarGames-KeyPress.wav</code>.</td>
+        </tr>
+        <tr>
+            <td>
+            <code>stroke_color</code>
+            </td>
+            <td>string</td>
+            <td>"green"</td>
+            <td>The color of the attack line.</td>
+        </tr>
+        <tr>
+            <td>
+            <code>stroke_width</code>
+            </td>
+            <td>number</td>
+            <td>2</td>
+            <td>The width of the attack line.</td>
+        </tr>
+        <tr>
+            <td>
+            <code>queue</code>
+            </td>
+            <td>number</td>
+            <td>7</td>
+            <td>The maximum number of attack lines that are drawn at a given time on the map.</td>
+        </tr>
+        <tr>
+            <td>
+            <code>limit</code>
+            </td>
+            <td>number</td>
+            <td>2</td>
+            <td>The maximum number of new lines that are drawn per refresh of the real-time data. If there are more new events than the <code>limit</code>, then the extra events are ignored. It doesn't make sense to have a <code>limit</code> greater than the <code>queue</code>. Also note that setting a <code>limit</code> too high will degrade drawning performances.</td>
         </tr>
     </tbody>
 </table>
